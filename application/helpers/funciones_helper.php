32d89cb7fd9cd6d844d85791758a5b91095cb92a
Mendoza789 <156829332+Mendoza789@users.noreply.github.com>
2026-09-12 02:29:49 -0600

    Delete application directory

--- application/helpers/funciones_helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    if ( !function_exists('formato_moneda')) {
-        function formato_moneda($cantidad=0) {
-            return '$'.number_format($cantidad,2,'.',',');
-        }//end navegacion_portal
-    }//end if !function_exists
